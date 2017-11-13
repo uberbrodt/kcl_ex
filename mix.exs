a7f2328065f68ca43fc28b78d0367d0c6ef56a85
Chris Brodt <chris@uberbrodt.net>
2017-11-12 18:31:01 -0600

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule KinesisClient.Mixfile do
   def project do
     [
       app: :kinesis_client,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       package: package(),
